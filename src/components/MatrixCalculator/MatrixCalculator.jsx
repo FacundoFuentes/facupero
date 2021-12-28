4ece4c7d3ec5e7c328f0b756ca55c486466414a4
Valentín Marchessi <vmarchessi@gmail.com>
2021-12-28 19:30:01 -0300

    debugging

--- src/components/MatrixCalculator/MatrixCalculator.jsx
+++ src/components/MatrixCalculator/MatrixCalculator.jsx
@@ -1,16 +1,21 @@
-import { MatrixDisplay } from "./MatrixDisplay/MatrixDisplay";
-import { MatrixForm } from "./MatrixForm/MatrixForm"
+import MatrixDisplay from "./MatrixDisplay/MatrixDisplay";
+import MatrixForm from "./MatrixForm/MatrixForm";
 import { FracMatrix } from "./algorithms/matrix";
+import style from "./MatrixCalculator.module.css";
 
 const test_matrix = new FracMatrix([
-	[1,0,0,0],
-	[1,1,0,0],
+	[1.2,0,0,3.1],
+	[1.5,1,0,0],
   	[0,1,1,0],
   	[0,0,0,1]
 ]);
 
 export default function MatrixCalculator() {
-    return (
-        <MatrixDisplay matrix={test_matrix}></MatrixDisplay>
+	return (
+		<div className={style.container}>
+        	<MatrixDisplay matrix={test_matrix}></MatrixDisplay>
+        	<MatrixDisplay matrix={test_matrix}></MatrixDisplay>
+        	<MatrixDisplay matrix={test_matrix}></MatrixDisplay>
+		</div>
     )
 }
