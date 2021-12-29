01f8bfbb71d453b42a51fb3548e4fbbec0d4e329
Valentín Marchessi <vmarchessi@gmail.com>
2021-12-29 13:17:09 -0300

    Added Matrix Operators

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -2,20 +2,65 @@ import { FracMatrix } from "../../components/MatrixCalculator/algorithms/matrix"
 
 const initialState = {
     matrix_calculator: {
-        formMatrices: {},
-        displayMatrix: [],
+        forms: {},
+        display: [],
     }
 }
 
 export default function rootReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'UPDATE_FORM_MATRICES':
-            return {
-                ...state,
-                matrix_calculator: {
-                    formMatrices: { ...state.matrix_calculator.formMatrices, [action.id]: action.payload}
+    switch (action.from) {
+        case 'MATRIX':
+            switch (action.type) {
+                case 'UPDATE_FORMS':
+                    return {
+                        ...state,
+                        matrix_calculator: {
+                            ...state.matrix_calculator,
+                            forms: { ...state.matrix_calculator.forms, [action.id]: action.payload}
+                        }
+                    }
+                case 'ADD':
+                    return {
+                        ...state,
+                        matrix_calculator: {
+                            ...state.matrix_calculator,
+                            display: FracMatrix.add(state.matrix_calculator.forms[0], state.matrix_calculator.forms[1])
+                        }
+                    }
+                case 'SUBSTRACT':
+                    return {
+                        ...state,
+                        matrix_calculator: {
+                            ...state.matrix_calculator,
+                            display: FracMatrix.substract(state.matrix_calculator.forms[0], state.matrix_calculator.forms[1])
+                        }
+                    }
+                case 'MULTIPLY':
+                    return {
+                        ...state,
+                        matrix_calculator: {
+                            ...state.matrix_calculator,
+                            display: FracMatrix.product(state.matrix_calculator.forms[0], state.matrix_calculator.forms[1])
+                        }
+                    }
+                case 'SWAP': {
+                    let obj = {};
+                    for (let i in state.matrix_calculator.forms){
+                        let rev = Object.values(state.matrix_calculator.forms).reverse();
+                        obj[i] = rev[i];
+                    }
+                    return {
+                        ...state,
+                        matrix_calculator: {
+                            ...state.matrix_calculator,
+                            forms: obj,
+                        }
+                    }
                 }
+                default:
+                    return state;
             }
+            
         default:
             return state;
     }
--- src/components/MatrixCalculator/MatrixDisplay/MatrixDisplay.jsx
+++ src/components/MatrixCalculator/MatrixDisplay/MatrixDisplay.jsx
@@ -2,35 +2,35 @@ import React from 'react';
 import 'katex/dist/katex.css';
 import MatrixParenthesis from '../MatrixParenthesis';
 import style from './MatrixDisplay.module.css';
+import { useSelector } from 'react-redux';
+import { FracMatrix } from '../algorithms/matrix';
 
-export default function MatrixDisplay({ matrix }) {
-	let rows = matrix.rows.map((row) =>
-		row.map((e, i) => {
-			if (e.den < 0) {
-				e.den = Math.abs(e.den);
-				e.num *= -1;
-			}
-			return e.den !== 1 ? <td key={i}>{`${e.num}/${e.den}`}</td> : <td key={i}>{e.num}</td>;
-		})
-	);
-
-	const table = rows.map((row, i) => {
-		return <tr key={i}>{row}</tr>;
-	});
-
-	const pYScaling = rows.length > 1 ? rows.length + 3: 1;
-	const parenthesisStyle = {
-		transform: `matrix(1.${pYScaling},0,0,${pYScaling},0,-10)`,
-		margin: '0px 10px',
+export default function MatrixDisplay() {
+	const matrix = useSelector(store => store.matrix_calculator.display)
+	let rows, table = null;
+	if (matrix instanceof FracMatrix) {
+		rows = matrix.rows.map((row) =>
+			row.map((e, i) => {
+				return e.den !== 1 ? <td key={i}>{`${e.num}/${e.den}`}</td> : <td key={i}>{e.num}</td>;
+			})
+		);
+		table = rows.map((row, i) => {
+			return <tr key={i}>{row}</tr>;
+		});
 	}
-
+	
 	return (
 		<div className={style.display}>
-			<MatrixParenthesis>
-			<table>
-				<tbody>{table}</tbody>
-			</table>
-			</MatrixParenthesis>
+			{table ? 
+				<MatrixParenthesis>
+					<table>
+						<tbody>{table}</tbody>
+					</table>
+				</MatrixParenthesis>
+				:
+				<span></span>
+			}
+
 		</div>
 	);
 }
