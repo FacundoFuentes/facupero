23709bda3bacc7263ace3171873b34285751cd11
Valentín Marchessi <vmarchessi@gmail.com>
2021-12-28 23:03:42 -0300

    Skeleton for matrix arithmetic

--- src/components/MatrixCalculator/MatrixCalculator.jsx
+++ src/components/MatrixCalculator/MatrixCalculator.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import MatrixDisplay from "./MatrixDisplay/MatrixDisplay";
 import MatrixForm from "./MatrixForm/MatrixForm";
 import { FracMatrix } from "./algorithms/matrix";
@@ -11,11 +13,35 @@ const test_matrix = new FracMatrix([
 ]);
 
 export default function MatrixCalculator() {
+	let counter = 1, default_size = 3;
+	const dispatch = useDispatch()
+
+	function add() {
+		console.log('Adding.')
+	}
+
+	function substract() {
+		console.log('Substracting.')
+	}
+
+	function multiply() {
+		console.log('Multiplying.')
+	}
+
+
 	return (
 		<div className={style.container}>
-        	<MatrixDisplay matrix={test_matrix}></MatrixDisplay>
-        	<MatrixDisplay matrix={test_matrix}></MatrixDisplay>
-        	<MatrixDisplay matrix={test_matrix}></MatrixDisplay>
+			<div className={style.form}>
+				<MatrixForm id={counter++} size={{ rows: default_size, cols: default_size }} />
+				<div className={style.buttons}>
+					<button onClick={add}>&#43;</button>
+					<button onClick={substract}>&#8722;</button>
+					<button onClick={multiply}>&times;</button>
+				</div>
+				<MatrixForm id={counter++} size={{ rows: default_size, cols: default_size }} />
+			</div>
+			<h1>=</h1>
+			<MatrixDisplay matrix={test_matrix}/>
 		</div>
     )
 }
