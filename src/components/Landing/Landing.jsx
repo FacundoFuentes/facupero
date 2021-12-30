487c797f9f5bac0dfe030aec0aeb0f005ae47336
Valentín Marchessi <vmarchessi@gmail.com>
2021-12-30 00:45:44 -0300

    added logo

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import landing from './Landing.module.css';
+import logo from './LogoFacupero.svg';
 
 export default function Landing() {
     return (
         <div className={landing.main}>
-            <h2>Welcome</h2>
-            <h1>Facupero</h1>
-            <Link to="/home">Explore</Link>
+            <img src={logo} alt='logo'></img>
+            <div className={landing.welcome}>
+                <h2>Welcome</h2>
+                <h1>Facupero</h1>
+                <Link to="/home">Explore</Link>
+            </div>
         </div>
     )
 }
